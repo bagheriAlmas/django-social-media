b7a552e9cf2a21c0cc0ba9d9bc52b97a3cc226cc
mahdibagheri <bagheri.almas@gmail.com>
2023-05-02 13:36:06 +0330

    start app profile

---
+++ profiles/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+    bio = models.TextField(max_length=200, blank=True, default='No Bio...')
+    email = models.EmailField(max_length=200, blank=True)
+    country = models.CharField(max_length=50, blank=True)
+    avatar = models.ImageField(default='avatar.png', upload_to='avatars/')
+    friends = models.ManyToManyField(User, blank=True, related_name='friends')
+    slug = models.SlugField(unique=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.created_at}'
