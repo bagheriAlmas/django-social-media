302fd88f523240684f85c7deef0ce77704af837a
mahdibagheri <bagheri.almas@gmail.com>
2023-05-03 12:26:22 +0330

    add profile urls to main urls

--- social_media/urls.py
+++ social_media/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import home_view
 from social_media import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home-view'),
+    path('profiles/', include('profiles.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
