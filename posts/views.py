5d2f1eadcb6761144690bcb4a7496aa70b11f58d
mahdibagheri <bagheri.almas@gmail.com>
2023-05-03 21:56:07 +0330

    create post list view function

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from posts.models import Post
+
+
+def post_list_view(request):
+    posts = Post.objects.all()
+    context = {
+        'posts': posts
+    }
+
+    return render(request, 'posts/post-list.html', context)
