c136d6547f8878f24afd996871207c264c28fcd8
mahdibagheri <bagheri.almas@gmail.com>
2023-05-03 21:55:44 +0330

    add posts app urls to main urls

--- social_media/urls.py
+++ social_media/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home-view'),
     path('profiles/', include('profiles.urls')),
+    path('posts/', include('posts.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
