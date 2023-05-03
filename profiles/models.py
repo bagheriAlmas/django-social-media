1e3b39a337687f9c150ad0da964e92f424741d8a
mahdibagheri <bagheri.almas@gmail.com>
2023-05-03 19:26:32 +0330

    create functions for : - get posts count - get all authors posts - get likes given count - get likes received count

--- profiles/models.py
+++ profiles/models.py
@@ -27,6 +27,30 @@ class Profile(models.Model):
     def get_friends_count(self):
         return self.friends.all().count()
 
+    def get_posts_count(self):
+        return self.posts.all().count()
+
+    def get_all_authors_posts(self):
+        return self.posts.all()
+
+    def get_likes_given_count(self):
+        likes = self.like_set.all()
+        total_liked = 0
+        for item in likes:
+            if item.value == 'like':
+                total_liked += 1
+        return total_liked
+
+    def get_likes_received_count(self):
+        posts = self.posts.all()
+        print(self.posts.all().count())
+        total_liked = 0
+        for item in posts:
+            print(item.like_set)
+            print(item.liked.all())
+            total_liked += item.like_set.all().count()
+        return total_liked
+
     def save(self, *args, **kwargs):
         ex = False
         if self.first_name and self.last_name:
