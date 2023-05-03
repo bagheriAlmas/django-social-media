e1ad464453a65ee423685b634e0629ec355b7a55
mahdibagheri <bagheri.almas@gmail.com>
2023-05-03 16:02:22 +0330

    change profile_view to update profile

--- profiles/views.py
+++ profiles/views.py
@@ -7,8 +7,16 @@ from .forms import ProfileModelForm
 def profile_view(request):
     profile = Profile.objects.get(user=request.user)
     form = ProfileModelForm(request.POST or None, request.FILES or None, instance=profile)
+    confirm = False
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            confirm = True
+
     context = {
         'profile': profile,
         'form': form,
+        'confirm': confirm,
     }
     return render(request, 'profiles/myprofile.html', context)
