2fd8b391b1a05183eae015954de14d0c3f48755d
mahdibagheri <bagheri.almas@gmail.com>
2023-05-03 13:13:30 +0330

    add form to update profile details

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from .models import Profile
+from .forms import ProfileModelForm
 
 
 # Create your views here.
 def profile_view(request):
     profile = Profile.objects.get(user=request.user)
+    form = ProfileModelForm(request.POST or None, request.FILES or None, instance=profile)
     context = {
-        'user': profile,
+        'profile': profile,
+        'form': form,
     }
     return render(request, 'profiles/myprofile.html', context)
