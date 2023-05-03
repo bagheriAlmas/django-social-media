f30f0066d86ea5d830feff1ab684a63db8e01190
mahdibagheri <bagheri.almas@gmail.com>
2023-05-03 12:26:45 +0330

    create profile_view to show profile detail

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Profile
+
 
 # Create your views here.
+def profile_view(request):
+    profile = Profile.objects.get(user=request.user)
+    context = {
+        'user': profile,
+    }
+    return render(request, 'profiles/myprofile.html', context)
