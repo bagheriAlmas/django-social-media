60ad4011f3aa7a260707aa63df1c0e4e84b6e133
mahdibagheri <bagheri.almas@gmail.com>
2023-05-05 15:26:49 +0330

    rewrite post_list_view to solve bug that conflict post_form and comment_form

--- posts/views.py
+++ posts/views.py
@@ -8,19 +8,35 @@ from .forms import PostModelForm, CommentModelForm
 def post_list_view(request):
     posts = Post.objects.all()
     profile = Profile.objects.get(user=request.user)
-    post_form = PostModelForm(request.POST or None, request.FILES or None)
-    comment_form = CommentModelForm(request.POST or None)
-    if post_form.is_valid():
-        instance = post_form.save(commit=False)
-        instance.author = profile
-        instance.save()
-        post_form = PostModelForm()
+
+    post_form = PostModelForm()
+    comment_form = CommentModelForm()
+    post_added = False
+
+    if 'btnPostSubmit' in request.POST:
+        print(request.POST)
+        post_form = PostModelForm(request.POST, request.FILES)
+        if post_form.is_valid():
+            instance = post_form.save(commit=False)
+            instance.author = profile
+            instance.save()
+            post_form = PostModelForm()
+            post_added = True
+    if 'btnCommentSubmit' in request.POST:
+        comment_form = CommentModelForm(request.POST)
+        if comment_form.is_valid():
+            instance = comment_form.save(commit=False)
+            instance.user = profile
+            instance.post = Post.objects.get(id=request.POST.get('post_id'))
+            instance.save()
+            comment_form = CommentModelForm()
 
     context = {
         'posts': posts,
         'profile': profile,
         'post_form': post_form,
         'comment_form': comment_form,
+        'post_added': post_added,
 
     }
     return render(request, 'posts/post-list.html', context)
