84bacb1c303464a93336fb0eafb78e0907311b98
mahdibagheri <bagheri.almas@gmail.com>
2023-05-05 12:24:50 +0330

    add like_unlike_post in urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import post_list_view
+from .views import post_list_view, like_unlike_post
 
 urlpatterns = [
-    path('', post_list_view, name='post-list')
+    path('', post_list_view, name='post-list'),
+    path('liked/', like_unlike_post, name='like-post-view'),
+
 ]
