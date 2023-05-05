4486535a55c005448762aa6253b0e8a42ebc2cde
mahdibagheri <bagheri.almas@gmail.com>
2023-05-05 12:24:32 +0330

    create method for like and unlike

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from posts.models import Post
+from posts.models import Post, Like
+from profiles.models import Profile
 
 
 def post_list_view(request):
@@ -8,5 +9,31 @@ def post_list_view(request):
     context = {
         'posts': posts
     }
-
     return render(request, 'posts/post-list.html', context)
+
+
+def like_unlike_post(request):
+    user = request.user
+    if request.method == 'POST':
+        post_id = request.POST.get('post_id')
+        post_obj = Post.objects.get(id=post_id)
+        profile = Profile.objects.get(user=user)
+
+        if profile in post_obj.liked.all():
+            post_obj.liked.remove(profile)
+        else:
+            post_obj.liked.add(profile)
+
+        like, create = Like.objects.get_or_create(user=profile, post_id=post_id)
+
+        if not create:
+            if like.value == 'like':
+                like.value = 'unlike'
+            else:
+                like.value = 'like'
+        else:
+            like.value = 'like'
+
+            post_obj.save()
+            like.save()
+    return redirect('post-list')
