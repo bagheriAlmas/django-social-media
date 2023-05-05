d70477c6fd1ee7f121bfd53c28cc962814abab59
mahdibagheri <bagheri.almas@gmail.com>
2023-05-05 14:17:38 +0330

    create form for comment

--- posts/forms.py
+++ posts/forms.py
@@ -3,6 +3,16 @@ from .models import Post, Comment
 
 
 class PostModelForm(forms.ModelForm):
+    content = forms.CharField(widget=forms.Textarea(attrs={'rows': 3}))
+
     class Meta:
         model = Post
         fields = ['content', 'image']
+
+
+class CommentModelForm(forms.ModelForm):
+    body = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'add a comment...'}))
+
+    class Meta:
+        model = Comment
+        fields = ['body', ]
