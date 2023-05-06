ea0cac6f662b904d6877df34b73d3cb4ca732781
mahdibagheri <bagheri.almas@gmail.com>
2023-05-06 21:39:52 +0330

    add send_invitation_view url

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import (profile_view, invites_received_view, available_profile_list_view,ProfileListView )
+from .views import (profile_view, invites_received_view, available_profile_list_view, ProfileListView,
+                    send_invitation_view)
 
 urlpatterns = [
     path('myprofile/', profile_view, name='my-profile-view'),
     path('myinvites/', invites_received_view, name='my-invites-view'),
     path('profiles-list/', ProfileListView.as_view(), name='all-profiles-view'),
     path('available-profiles-list/', available_profile_list_view, name='available-profiles-view'),
+    path('send-invite/', send_invitation_view, name='send-invite'),
 ]
