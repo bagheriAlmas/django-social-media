938e54e165fc49c7821cb43067535b7b5e13a9da
mahdibagheri <bagheri.almas@gmail.com>
2023-05-06 20:37:30 +0330

    update ProfileListView url

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import profile_view, invites_received_view, profile_list_view, available_profile_list_view
+from .views import (profile_view, invites_received_view, available_profile_list_view,ProfileListView )
 
 urlpatterns = [
     path('myprofile/', profile_view, name='my-profile-view'),
     path('myinvites/', invites_received_view, name='my-invites-view'),
-    path('profiles-list/', profile_list_view, name='all-profiles-view'),
+    path('profiles-list/', ProfileListView.as_view(), name='all-profiles-view'),
     path('available-profiles-list/', available_profile_list_view, name='available-profiles-view'),
 ]
