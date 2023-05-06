d581d7aac1d7d5846a89ceab3ccf5cce9e7f9330
mahdibagheri <bagheri.almas@gmail.com>
2023-05-06 15:15:59 +0330

    add update url and delete url in urls

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import post_list_view, like_unlike_post
+from .views import post_list_view, like_unlike_post, PostDeleteView, PostUpdateView
 
 urlpatterns = [
     path('', post_list_view, name='post-list'),
     path('liked/', like_unlike_post, name='like-post-view'),
-
+    path('<pk>/delete/', PostDeleteView.as_view(), name='delete-view'),
+    path('<pk>/update/', PostUpdateView.as_view(), name='update-view'),
 ]
