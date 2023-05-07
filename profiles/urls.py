67f0f6adb4f23de5619d49ec62412f32d29f083f
mahdibagheri <bagheri.almas@gmail.com>
2023-05-07 19:20:13 +0330

    add ProfileDetailView class to urls.py and make template for this view

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path
 from .views import (profile_view, invites_received_view, available_profile_list_view, ProfileListView,
-                    send_invitation_view, remove_from_friends_view, accept_invitation, reject_invitation)
+                    send_invitation_view, remove_from_friends_view, accept_invitation, reject_invitation,
+                    ProfileDetailView)
 
 urlpatterns = [
+    path('', ProfileListView.as_view(), name='all-profiles-view'),
     path('myprofile/', profile_view, name='my-profile-view'),
-    path('myinvites/', invites_received_view, name='my-invites-view'),
-    path('profiles-list/', ProfileListView.as_view(), name='all-profiles-view'),
+    path('<slug>/', ProfileDetailView.as_view(), name='profiles-detail-view'),
     path('available-profiles-list/', available_profile_list_view, name='available-profiles-view'),
     path('send-invite/', send_invitation_view, name='send-invite'),
     path('remove-friend/', remove_from_friends_view, name='remove-friend'),
-    path('myinvites/accept', accept_invitation, name='accept-invites-view'),
-    path('myinvites/reject', reject_invitation, name='reject-invites-view'),
+    path('myinvites/', invites_received_view, name='my-invites-view'),
+    path('myinvites/accept/', accept_invitation, name='accept-invites-view'),
+    path('myinvites/reject/', reject_invitation, name='reject-invites-view'),
 
 ]
