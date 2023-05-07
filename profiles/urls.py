0154bc22ac20fe5fbc0cfda8e19c4a6674b7ea4a
mahdibagheri <bagheri.almas@gmail.com>
2023-05-07 20:29:34 +0330

    fix bug : remove extra / in endpoints

--- profiles/urls.py
+++ profiles/urls.py
@@ -8,10 +8,10 @@ urlpatterns = [
     path('myprofile/', profile_view, name='my-profile-view'),
     path('<slug>/', ProfileDetailView.as_view(), name='profiles-detail-view'),
     path('available-profiles-list/', available_profile_list_view, name='available-profiles-view'),
-    path('send-invite/', send_invitation_view, name='send-invite'),
-    path('remove-friend/', remove_from_friends_view, name='remove-friend'),
+    path('send-invite', send_invitation_view, name='send-invite'),
+    path('remove-friend', remove_from_friends_view, name='remove-friend'),
     path('myinvites/', invites_received_view, name='my-invites-view'),
-    path('myinvites/accept/', accept_invitation, name='accept-invites-view'),
-    path('myinvites/reject/', reject_invitation, name='reject-invites-view'),
+    path('myinvites/accept', accept_invitation, name='accept-invites-view'),
+    path('myinvites/reject', reject_invitation, name='reject-invites-view'),
 
 ]
