2ebe4408b4fb1ad8191effea071ea8767ccf95a4
mahdibagheri <bagheri.almas@gmail.com>
2023-05-07 12:21:02 +0330

    - add url of accept_invitation_view and reject_invitation_view - add urls to template

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import (profile_view, invites_received_view, available_profile_list_view, ProfileListView,
-                    send_invitation_view, remove_from_friends_view)
+                    send_invitation_view, remove_from_friends_view, accept_invitation, reject_invitation)
 
 urlpatterns = [
     path('myprofile/', profile_view, name='my-profile-view'),
@@ -9,4 +9,7 @@ urlpatterns = [
     path('available-profiles-list/', available_profile_list_view, name='available-profiles-view'),
     path('send-invite/', send_invitation_view, name='send-invite'),
     path('remove-friend/', remove_from_friends_view, name='remove-friend'),
+    path('myinvites/accept', accept_invitation, name='accept-invites-view'),
+    path('myinvites/reject', reject_invitation, name='reject-invites-view'),
+
 ]
