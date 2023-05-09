367bc3a4f34f71a1ba062dcd83d4e7cee9b35040
mahdibagheri <bagheri.almas@gmail.com>
2023-05-09 16:42:05 +0330

    set LOGIN_URL to admin

--- social_media/settings.py
+++ social_media/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
 
     'fontawesomefree'
 ]
-
+LOGIN_URL = '/admin'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
