31f95fea646e4d8ced239c18ad481fdaa49b4a39
mahdibagheri <bagheri.almas@gmail.com>
2023-05-09 19:30:37 +0330

    fix bug , we must return count of invitation_received

--- profiles/context_processor.py
+++ profiles/context_processor.py
@@ -11,6 +11,6 @@ def profile_pic(request):
 def invitation_receive_number(request):
     if request.user.is_authenticated:
         profile = Profile.objects.get(user=request.user)
-        post_count = Relationship.objects.invitation_received(profile)
+        post_count = Relationship.objects.invitation_received(profile).count()
         return {'invites_num': post_count}
     return {}
