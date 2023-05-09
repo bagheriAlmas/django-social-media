8bd94eee318252f1800a4200ffa7077c8a1dffec
mahdibagheri <bagheri.almas@gmail.com>
2023-05-09 18:57:27 +0330

    create function to return number of invitation_received

--- profiles/context_processor.py
+++ profiles/context_processor.py
@@ -8,4 +8,9 @@ def profile_pic(request):
     return {}
 
 
-
+def invitation_receive_number(request):
+    if request.user.is_authenticated:
+        profile = Profile.objects.get(user=request.user)
+        post_count = Relationship.objects.invitation_received(profile)
+        return {'invites_num': post_count}
+    return {}
