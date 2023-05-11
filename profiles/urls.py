4790e783885f70e4bfcb4341df802fbad38e1dc8
mahdibagheri <bagheri.almas@gmail.com>
2023-05-11 15:03:01 +0330

    fix bug in urls.py

--- profiles/urls.py
+++ profiles/urls.py
@@ -6,12 +6,12 @@ from .views import (profile_view, invites_received_view, available_profile_list_
 urlpatterns = [
     path('', ProfileListView.as_view(), name='all-profiles-view'),
     path('myprofile/', profile_view, name='my-profile-view'),
-    path('<slug>/', ProfileDetailView.as_view(), name='profiles-detail-view'),
     path('available-profiles-list/', available_profile_list_view, name='available-profiles-view'),
     path('send-invite', send_invitation_view, name='send-invite'),
     path('remove-friend', remove_from_friends_view, name='remove-friend'),
     path('myinvites/', invites_received_view, name='my-invites-view'),
     path('myinvites/accept', accept_invitation, name='accept-invites-view'),
     path('myinvites/reject', reject_invitation, name='reject-invites-view'),
+    path('<slug:slug>/', ProfileDetailView.as_view(), name='profiles-detail-view'),
 
 ]
