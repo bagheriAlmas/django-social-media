b305bee3ca6716d4d78f7dd96fe6f0e5f07866d5
mahdibagheri <bagheri.almas@gmail.com>
2023-05-13 17:57:20 +0330

    send receiver and sender relationships data in post_list_view to show suggestions profiles

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,5 @@
+import random
+
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -7,14 +9,18 @@ from django.urls import reverse_lazy
 from django.views.generic import DeleteView, UpdateView
 
 from posts.models import Post, Like
-from profiles.models import Profile
-from .forms import PostModelForm, CommentModelForm
+from profiles.models import Profile, Relationship
+from .forms import PostModelForm, CommentModelForm, ImageForm
 
 
 @login_required
 def post_list_view(request):
     posts = Post.objects.all()
     profile = Profile.objects.get(user=request.user)
+
+    user = request.user
+    suggestions = Profile.objects.get_all_available_profiles_to_invite(user)
+    random.shuffle(suggestions)
 
     post_form = PostModelForm()
     comment_form = CommentModelForm()
@@ -38,13 +44,30 @@ def post_list_view(request):
             instance.save()
             comment_form = CommentModelForm()
 
+    # ---------------------------------
+
+    relationship_receiver_object = Relationship.objects.filter(sender=profile)
+    relationship_sender_object = Relationship.objects.filter(receiver=profile)
+
+    relationship_receiver_list = []
+    relationship_sender_list = []
+
+    for item in relationship_receiver_object:
+        relationship_receiver_list.append(item.receiver.user)
+    for item in relationship_sender_object:
+        relationship_sender_list.append(item.sender.user)
+
+    # ---------------------------------
+
     context = {
         'posts': posts,
         'profile': profile,
         'post_form': post_form,
         'comment_form': comment_form,
         'post_added': post_added,
-
+        'suggestions': suggestions[:5],
+        'relationship_receiver': relationship_receiver_list,
+        'relationship_sender': relationship_sender_list,
     }
     return render(request, 'posts/post-list.html', context)
 
@@ -109,3 +132,18 @@ class PostUpdateView(LoginRequiredMixin, UpdateView):
         else:
             form.add_error(None, "You need to be the author of the post in order to update this post")
             return super().form_valid(form)
+
+
+def index(request):
+    form = ImageForm(request.POST or None, request.FILES or None)
+    profile = Profile.objects.get(user=request.user)
+
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.author = profile
+        instance.save()
+        return JsonResponse({'message': 'works'})
+    context = {
+        'form': form
+    }
+    return render(request, 'test/test.html', context)
